93277b7de2f8e0538f8d96bfeb1a6459101f00ff
LilasAll <lilas.allard@gmail.com>
2019-11-14 13:17:52 +0100

    first commit

---
+++ script_2.js
@@ -0,0 +1,10 @@
+let number = prompt('De quel nombre veux-tu calculer la factorielle?')
+
+
+let count = 0
+let factoriel = 0 
+
+while number >= count {
+	let factoriel = count * number
+	let count = count + 1
+}
